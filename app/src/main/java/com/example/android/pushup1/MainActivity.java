a43d7b23c6fa016d43a69946aa844d15e66de1ec
mohansumi <sumangalymohanasundram@gmail.com>
2019-03-04 08:44:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/pushup1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.pushup1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    private void testUpload(){
+        Toast.makeText(this, "This is test", Toast.LENGTH_SHORT).show();
+    }
+}
